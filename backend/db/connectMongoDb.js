65b9ed90517267e5394e5923290b2ba296fca9a2
Shariif-Rajpoot <mr8781256@gmail.com>
2024-11-11 00:47:03 -0800

    Set Mongodb

---
+++ backend/db/connectMongoDb.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectMongoDb = async () => {
+  console.log("IN MONGOOS FUNCTION");
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`Mongodb connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(`Error connection to mongodb: ${error.message}`);
+    process.exit(1);
+  }
+};
+export default connectMongoDb;
