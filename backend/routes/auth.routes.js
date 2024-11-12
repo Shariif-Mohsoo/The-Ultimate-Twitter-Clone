fa378d9e86b5c1ad7ada8049ff252aea4f0bbb06
Shariif-Rajpoot <mr8781256@gmail.com>
2024-11-12 08:28:18 -0800

    Auth Routes

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,7 @@
+import express from "express";
+const router = express.Router();
+import { login, logout, signup } from "../controllers/auth.controller.js";
+router.get("/signup", signup);
+router.get("/login", login);
+router.get("/logout", logout);
+export default router;
