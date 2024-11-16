095880193275468962a5d1ebcd8903f7c8e83b02
Shariif-Rajpoot <mr8781256@gmail.com>
2024-11-16 12:28:52 -0800

    Server Set Up

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// middleware
 app.use("/api/auth", authRoutes);
+// for json request // to parse req.body
+app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Listening at port ${PORT}`);
