bead8fb45b0fc7333b9072d847ff1b6ca0753baf
Shariif-Rajpoot <mr8781256@gmail.com>
2024-11-16 09:40:47 -0800

    Server Set

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,11 @@ import authRoutes from "./routes/auth.routes.js";
 import connectMongoDb from "./db/connectMongoDb.js";
 
 dotenv.config();
-console.log(process.env.MONGO_URI);
+// console.log(process.env.MONGO_URI);
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use("/api/auth", authRoutes);
-app.get("/", (req, res) => {
-  res.send("You are welcome dear");
-});
 
 app.listen(PORT, () => {
   console.log(`Listening at port ${PORT}`);
