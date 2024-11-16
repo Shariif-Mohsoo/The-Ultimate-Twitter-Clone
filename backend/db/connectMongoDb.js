9559623cf410cfe277d1192f7ead28ec67eecb89
Shariif-Rajpoot <mr8781256@gmail.com>
2024-11-16 09:42:23 -0800

    Function To Connect Mongodb via mongoose

--- backend/db/connectMongoDb.js
+++ backend/db/connectMongoDb.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
-
 const connectMongoDb = async () => {
-  console.log("IN MONGOOS FUNCTION");
+  // console.log("IN MONGOOS FUNCTION");
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`Mongodb connected: ${conn.connection.host}`);
