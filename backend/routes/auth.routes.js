e593b95b3027a749fb420ec6cfa66d5fafd9c754
Shariif-Rajpoot <mr8781256@gmail.com>
2024-11-17 11:54:11 -0800

    Routes Setup

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,7 +1,21 @@
 import express from "express";
+
+import { protectRoute } from "../middleware/protectRoute.js";
+import {
+  getMe,
+  login,
+  logout,
+  signup,
+} from "../controllers/auth.controller.js";
+
 const router = express.Router();
-import { login, logout, signup } from "../controllers/auth.controller.js";
-router.get("/signup", signup);
-router.get("/login", login);
-router.get("/logout", logout);
+
+// to check whether the user is authorized or not.
+router.get("/me", protectRoute, getMe);
+// to signup for the user
+router.post("/signup", signup);
+// to login the user
+router.post("/login", login);
+// to logout the user.
+router.post("/logout", logout);
 export default router;
