bde77b57ec17059c11cd94b06491758826440871
Shariif-Rajpoot <mr8781256@gmail.com>
2024-11-18 07:57:00 -0800

    Entry Point With All Middleware and Routes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectMongoDb from "./db/connectMongoDb.js";
 
 dotenv.config();
@@ -18,6 +20,7 @@ app.use(express.urlencoded({ extended: true })); // Parses URL-encoded data
 app.use(cookieParser()); //to get token we created for cookie.
 // middleware
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Listening at port ${PORT}`);
